daf81f72cc37449e19304cb9c48dad7aad3d6742
zhaoguoan <zhaoguoan@ultrarisc.com>
2026-08-21 15:34:53 +0800

    Respect Android system bar insets

--- android/app/src/main/java/com/zga/dualnback/MainActivity.java
+++ android/app/src/main/java/com/zga/dualnback/MainActivity.java
@@ -1,5 +1,29 @@
 package com.zga.dualnback;
 
+import android.os.Bundle;
+
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowCompat;
+import androidx.core.view.WindowInsetsControllerCompat;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ViewCompat.setOnApplyWindowInsetsListener(
+            getBridge().getWebView(),
+            (view, windowInsets) -> windowInsets
+        );
+        ViewCompat.requestApplyInsets(getBridge().getWebView());
+
+        WindowInsetsControllerCompat insetsController = WindowCompat.getInsetsController(
+            getWindow(),
+            getWindow().getDecorView()
+        );
+        insetsController.setAppearanceLightStatusBars(true);
+        insetsController.setAppearanceLightNavigationBars(true);
+    }
+}
